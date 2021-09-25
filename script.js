0c7b814cdefeea520e2aa960cfdec697b872bb11
roman <romka94shinkarenko@gmail.com>
2021-09-25 20:34:39 +0300

    test password

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('hi i am JS');
+const LOGIN = document.querySelector(".login");
+const PASSWORD = document.querySelector(".pass");
+const GO = document.querySelector(".go");
+
+GO.addEventListener('click', () => {
+    let login = LOGIN.value;
+    let password = PASSWORD.value;
+    if(login == "shin&klem" & password == "123"){
+        alert("good!");
+    }else{
+        alert("no");
+    }
+});
