b20ce79c438acc223c0b87f4a6eb4951305a4173
roman <romka94shinkarenko@gmail.com>
2021-09-26 13:40:06 +0300

    test v0.2

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ console.log('hi i am JS');
 const LOGIN = document.querySelector(".login");
 const PASSWORD = document.querySelector(".pass");
 const GO = document.querySelector(".go");
+const DIVPASSWORD = document.querySelector(".password");
+const PODPAS = document.querySelector(".podpas");
+const ONEH = document.querySelector(".oneH");
 
 GO.addEventListener('click', () => {
     let login = LOGIN.value;
     let password = PASSWORD.value;
     if(login == "shin&klem" & password == "123"){
-        alert("good!");
+        DIVPASSWORD.style.animation = "goodCheck 1s linear 0s forwards, chekDvig 1s linear 1s forwards";
+        PODPAS.style.display = "none";
+        ONEH.style.animation = "notHel 1s linear 0s forwards";
     }else{
-        alert("no");
+        DIVPASSWORD.style.animation = "notCheck 0.5s linear 0s forwards";
     }
 });
