7b68d928bca7e73dd81038809f864d017106cfe5
roman <romka94shinkarenko@gmail.com>
2021-09-27 22:11:05 +0300

    test v0.3

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const PODPAS = document.querySelector(".podpas");
 const ONEH = document.querySelector(".oneH");
 const LBL = document.querySelector(".lbl");
 const WRAPPER = document.querySelector(".wrapper");
+const HED = document.querySelector(".hed");
 
 GO.addEventListener('click', () => {
     let login = LOGIN.value;
@@ -18,6 +19,7 @@ GO.addEventListener('click', () => {
         ONEH.style.animation = "notHel 1s linear 0s forwards";
         LBL.style.animation = "LBLcenter 0.1s linear 2s forwards";
         WRAPPER.style.backgroundImage =  "linear-gradient(45deg, black 0%, black 100%)";
+        HED.style.animation = "hedVisabiliti 1s linear 1.5s forwards";
     }else{
         DIVPASSWORD.style.animation = "notCheck 0.5s linear 0s forwards";
     }
